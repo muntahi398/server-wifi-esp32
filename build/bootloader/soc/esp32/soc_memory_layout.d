fb2cccaf3cac8cf023d422834d0712189293457a
muntahi398 <md_nurul.karim@dfki.de>
2018-08-22 15:46:10 +0200

    deleting temporary files

--- build/bootloader/soc/esp32/soc_memory_layout.d
+++
@@ -1,3 +0,0 @@
-esp32/soc_memory_layout.o: \
- /home/muntahi/esp/esp-idf/components/soc/esp32/soc_memory_layout.c
-/home/muntahi/esp/esp-idf/components/soc/esp32/soc_memory_layout.c:
--- build/jsmn/src/jsmn.d
+++
@@ -1,5 +0,0 @@
-src//jsmn.o: /home/muntahi/esp/esp-idf/components/jsmn/src/jsmn.c \
- /home/muntahi/esp/esp-idf/components/jsmn/include/jsmn.h
-
-/home/muntahi/esp/esp-idf/components/jsmn/include/jsmn.h:
-/home/muntahi/esp/esp-idf/components/jsmn/src//jsmn.c:
